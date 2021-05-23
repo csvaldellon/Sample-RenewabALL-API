39e03c0915585dfe3f9c13553ecf3b583034197d
csvaldellon <76705051+csvaldellon@users.noreply.github.com>
2021-05-23 19:23:18 +0800

    Add files via upload

---
+++ ML Load 3 DOE.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import pandas as pd
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    predLuzon, predVisayas, predMindanao = 0, 0, 0
+    if request.method == "POST":
+        userDate = request.form.get("date")
+        predLuzon, predVisayas, predMindanao = get_predict(userDate)
+    return render_template("RenewabALL_2.html", predLuzon=predLuzon, predVisayas=predVisayas, predMindanao=predMindanao)
+
+
+# @app.route("/predict", methods=["GET", "POST"])
+def get_predict(userDate):
+    model = pd.read_csv("Peak_Demand_Predictions.csv")
+    # print(model)
+
+    luzonValue = round(model["Luzon"][model["Date"] == userDate].values[0])
+    visayasValue = round(model["Visayas"][model["Date"] == userDate].values[0])
+    mindanaoValue = round(model["Mindanao"][model["Date"] == userDate].values[0])
+
+    return luzonValue, visayasValue, mindanaoValue
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
